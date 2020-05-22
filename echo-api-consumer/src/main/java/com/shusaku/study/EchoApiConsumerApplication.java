baf7700ce651b4f2cb46ec019a132ebf580e9c17
liuzp <1131664758@qq.com>
2020-05-22 16:31:27 +0800

    dubbo入门学习　java-api方式 consumer

---
+++ echo-api-consumer/src/main/java/com/shusaku/study/EchoApiConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.shusaku.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EchoApiConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EchoApiConsumerApplication.class, args);
+    }
+
+}
